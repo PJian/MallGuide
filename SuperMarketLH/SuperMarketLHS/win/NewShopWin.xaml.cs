ad11fbfd06f1fe5fcd3f39a99fce8633a7a1cd06
peijian <peijian214@126.com>
2016-04-16 09:59:49 +0800

    add right btn func on map edit

---
+++ SuperMarketLH/SuperMarketLHS/win/NewShopWin.xaml.cs
@@ -0,0 +1,31 @@
+﻿using EntityManagementService.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SuperMarketLHS.win
+{
+    /// <summary>
+    /// NewShopWin.xaml 的交互逻辑
+    /// 完成商铺的新建和入驻功能
+    /// </summary>
+    public partial class NewShopWin : Window
+    {
+        private Obstacle relativeObstacle;
+        public NewShopWin(Obstacle relativeObstacle)
+        {
+            InitializeComponent();
+            this.relativeObstacle = relativeObstacle;
+        }
+
+    }
+}
---
+++ SuperMarketLH/SuperMarketLHS/win/ShopInWin.xaml.cs
@@ -0,0 +1,32 @@
+﻿using EntityManagementService.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SuperMarketLHS.win
+{
+    /// <summary>
+    /// ShopInWin.xaml 的交互逻辑
+    /// 完成商铺的入驻
+    /// </summary>
+    public partial class ShopInWin : Window
+    {
+       
+        private Obstacle relativeObstacle;
+        public ShopInWin(Obstacle relativeObstacle)
+        {
+            InitializeComponent();
+          
+            this.relativeObstacle = relativeObstacle;
+        }
+    }
+}
